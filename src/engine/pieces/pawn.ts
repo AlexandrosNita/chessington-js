f2bcabadb2f56536573d0f185a7cb9b1cbc50b0a
Alexandros Nita <Alexandros.Nita@softwire.com>
2025-07-07 12:11:21 +0300

    Solved Red-1

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,13 +1,29 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
+    public hasFirstMove: boolean = true;
+
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board): Square[] {
+        const currentPosition = board.findPiece(this);
+        const availableOffsets = this.choosePossibleOffsets();
+        const result: Square[] = [];
+        for (let num of availableOffsets) {
+            result.push(new Square(currentPosition.row + num, currentPosition.col));
+        }
+        return result;
+    }
+
+    private choosePossibleOffsets(): number[] {
+        if (this.hasFirstMove) {
+            return (this.player === Player.WHITE ? [1, 2] : [-1, -2]);
+        }
+        return (this.player === Player.WHITE ? [1] : [-1]);
     }
 }
